f58cc865dab6ab56ae8ee79df96f858c95e323fd
paul <paulzhang19921113@gmail.com>
2019-01-23 07:32:51 -0500

    Updated columns name typo in player_info.csv

--- Assignment_1/raw/rename.py
+++ Assignment_1/raw/rename.py
@@ -6,8 +6,14 @@ cwd = os.getcwd()
 
 player_info=pd.read_csv(cwd+'/player_info.csv')
 
-player_info.columns = ['NAME','YEAR_START','YAR_END',"POSITION",'HEIGHT','WEIGHT','BIRTH_DATE','COLLEGE']
+#player_info = player_info.drop(player_info.columns[0], axis=1)
 
+print(player_info.columns)
+
+player_info.columns = ['FULL_NAME','YEAR_START','YEAR_END',"POSITION",'HEIGHT','WEIGHT','BIRTH_DATE','COLLEGE']
+
+
+#player_info = player_info.drop(player_info.columns[0], axis=1)
 # csv file path
 csv_path = cwd + '/player_info.csv'
 
