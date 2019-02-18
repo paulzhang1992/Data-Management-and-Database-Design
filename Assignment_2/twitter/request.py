2cf54c1e7fc41b817ae189464c8dfe39f131b6ad
paulzhang1992 <paulzhang19921113@gmail.com>
2019-02-18 16:39:33 -0500

    Added commenting for team request

--- Assignment_2/twitter/request.py
+++ Assignment_2/twitter/request.py
@@ -4,25 +4,39 @@ import pandas as pd
 
 
 def team_tweet():
+    # Created data frame to store the tweets
     tweets_team = pd.DataFrame(columns=["CREATED_AT", "TWEET_ID", "TWEET", "USER_ID", "USER_NAME", "RETWEET_COUNT",
                                       "FAVORITE_COUNT", "HASHTAGS"])
+    # Requesting NBA team info from a twitter list from NBA official twitter
     nba_teams = api.list_members('NBA', 'nbateams',count=30)
     team_twitter = pd.DataFrame(columns=["ACCOUNT_ID", "TEAM", "TWITTER_ACC"])
     temp_team = team_twitter.copy()
+    '''
+    Iterate through the members and extract useful information
+    1. account id for each team
+    2. team name inculding city and name for each team
+    3. screen name for accecc the team twitter page
+    '''
     for member in nba_teams:
         temp_team.at[0, "ACCOUNT_ID"] = member.id
         temp_team.at[0, "TEAM"] = member.name
         temp_team.at[0, "TWITTER_ACC"] = member.screen_name
         team_twitter = team_twitter.append(temp_team)
+    # Store the list to csv
     team_twitter = team_twitter.reset_index(drop=True)
+    # Iterate through each team and scrape their tweets
     for i in range(0, team_twitter.shape[0]):
         account = team_twitter.at[i, "TWITTER_ACC"]
+        # Use TweetProcress to modify the team tweet object
         current_team = TweetProcress()
         current_team_tweets = current_team.request_tweet(screen_name=account, count=200, pages=25)
+        # returns the tweet data frame
         tweets_team = tweets_team.append(current_team_tweets)
         file = "Tweet "+team_twitter.at[i,"TEAM"]+".csv"
+        # Store tweets for each team
         current_team_tweets.to_csv("rawData//team//"+file, index=False)
         print(file+" is done")
+    # One single csv contains all the tweets
     tweets_team.to_csv("rawData//team//tweets_team.csv", index=False)
 
 
@@ -40,10 +54,12 @@ def player_tweet():
         print(file+" is done")
     tweets_player.to_csv("rawData//player//tweets_player.csv", index=False)
 
+def fan_tweet():
+
 
 if __name__ == "__main__":
-    # team_tweet()
-    player_tweet()
+    #team_tweet()
+    #player_tweet()
 
 
 
