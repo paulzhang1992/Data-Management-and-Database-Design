bf1e3a2db8a5f7e7cc5f9651c5dde18742bdc509
paulzhang1992 <paulzhang19921113@gmail.com>
2019-02-22 14:52:01 -0500

    test

--- Assignment_2/database/player_id_append_from_player_info.sql
+++ Assignment_2/database/player_id_append_from_player_info.sql
@@ -27,3 +27,38 @@ INSERT INTO player_id (FIRST_NAME, FULL_NAME, LAST_NAME, PLAYER_ID)
 SELECT * FROM player_id_append;
 
 Drop VIEW player_null, player_id_append;
+
+-------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE VIEW temp AS select * from draft_history;
+
+DELETE temp
+    FROM temp LEFT JOIN
+         (SELECT min(DRAFT_ID) AS id
+          FROM temp
+          GROUP BY PLAYER_ID
+         ) tokeep
+         ON temp.DRAFT_ID = tokeep.id
+    WHERE tokeep.id IS NULL;
+
+CREATE OR REPLACE VIEW player_null AS
+  SELECT * FROM(
+    SELECT SUBSTRING_INDEX(draft_history.PLAYER_NAME, ' ', 1) AS FIRST_NAME,
+           draft_history.PLAYER_NAME,
+           SUBSTRING_INDEX(draft_history.PLAYER_NAME, ' ', -1) AS LAST_NAME,
+           draft_history.PLAYER_ID,player_id.FULL_NAME AS NAME
+     FROM draft_history
+     LEFT JOIN player_id
+       ON player_id.PLAYER_ID = draft_history.PLAYER_ID) as temp
+     WHERE temp.NAME IS NULL;
+
+CREATE OR REPLACE VIEW player_id_append AS
+  SELECT FIRST_NAME,PLAYER_NAME AS FULL_NAME,LAST_NAME,PLAYER_ID FROM player_null;
+
+INSERT INTO player_id (FIRST_NAME, FULL_NAME, LAST_NAME, PLAYER_ID)
+SELECT * FROM player_id_append;
+
+Drop VIEW player_null, player_id_append, temp;
+
+SELECT * FROM player_id
+GROUP BY PLAYER_ID HAVING COUNT(*) > 1;
--- Assignment_2/twitter/rawData/tweets_edit.py
+++ Assignment_2/twitter/rawData/tweets_edit.py
@@ -12,45 +12,56 @@ To-Do:
 
 
 def extract_hashtags (current_row=pd.Series, dataframe=pd.DataFrame):
+    # temp data frame
     temp = pd.DataFrame(columns=["TWEET_ID", "HASHTAG"])
+    # Check if there is hashtags
     if pd.notna(current_row["HASHTAGS"]):
+        # If true iterate the hashtags list converted with ast package
         hashtags = ast.literal_eval(current_row["HASHTAGS"])
         for hashtag in hashtags:
             temp.at[0, "TWEET_ID"] = current_row["TWEET_ID"]
             temp.at[0,"HASHTAG"] = hashtag
-
             dataframe = dataframe.append(temp)
+    else :
+        # If not write NO_HASHTAG along with tweet id
+        temp.at[0, "TWEET_ID"] = current_row["TWEET_ID"]
+        temp.at[0, "HASHTAG"] = "NO_HASHTAG" 
+        dataframe = dataframe.append(temp)
     return dataframe
 
 
 if __name__ == "__main__":
-    '''
+    # Load tweets from 3 files to dataframe
     tweets_team = pd.read_csv("team//tweets_team.csv")
     tweets_player = pd.read_csv("player//tweets_player.csv")
     tweets_fan = pd.read_csv("fan//tweets_fan.csv")
+    # Assign each tweet a type of entries
     tweets_team["TYPE"] = "team"
     tweets_player["TYPE"] = "player"
     tweets_fan["TYPE"] = "fan"
+    # Combine them into a single file
     tweet = tweets_team.append(tweets_player.append(tweets_fan))
-    print(tweets_team.shape)
-    tweet.to_csv("tweets.csv", index = False)
-    '''
-    j = 0
-    hashtag_df = pd.read_csv("hashtags.csv")
-    tweet = pd.read_csv("tweets.csv")
+    if tweets_team.shape[0] == tweets_team.shape[0]+tweets_player.shape[0]+tweets_fan.shape[0]
+        print("success")
+        tweet.to_csv("tweets.csv", index=False)
+    else:print("Found missing values during the merge")
 
+    # Start to extract hashtags to new single dataframe
+    hashtag_df = pd.DataFrame(columns=["TWEET_ID", "HASHTAG"])
+    # j for counting the progress
+    j = 0
     for index, row in tweet.iterrows():
-        #hashtag_df = pd.read_csv("hashtags.csv")
+        # Data frame to store the value of current row
         current_row_hashtag = pd.DataFrame(columns=["TWEET_ID", "HASHTAG"])
+        # Assign hashtags info
         current_row_hashtag = (extract_hashtags(row, current_row_hashtag))
         hashtag_df = hashtag_df.append(current_row_hashtag)
 
-        if j%50 == 0:
+        if j%1000 == 0:
             print(j)
             print(tweet.shape[0])
         j += 1
-
-        #hashtag_df.to_csv("hashtags.csv", index = False)
+    # Reset the index and adding it when saving file
     hashtag_df = hashtag_df.reset_index(drop=True)
-    hashtag_df.to_csv("hashtags.csv")
+    hashtag_df.to_csv("hashtags_null.csv")
     print("Finished")
