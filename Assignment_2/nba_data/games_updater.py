9ae2cfcad4c758caadd120de1dc46a2c374f5dab
paulzhang1992 <paulzhang19921113@gmail.com>
2019-03-01 22:12:40 -0500

    Finished day by day team and player stats request

--- Assignment_2/nba_data/games_updater.py
+++ Assignment_2/nba_data/games_updater.py
@@ -1,5 +1,6 @@
 import datetime
 from nba_api.stats.endpoints import scoreboardv2
+from nba_api.stats.endpoints import leaguegamefinder
 from nba_api.stats.endpoints import boxscoretraditionalv2
 import pandas as pd
 
@@ -18,27 +19,33 @@ Update database after each day:
 
 class TeamStats:
     def __init__(self):
-        self.date = datetime.datetime.now() - datetime.timedelta(1)
+        self.yesterday = datetime.datetime.now() - datetime.timedelta(1)
+        self.game_date = str(self.yesterday.year) + "-" + str(self.yesterday.month) + "-" + str(self.yesterday.day)
+        self.games = pd.DataFrame(columns=[])
 
     def gamefinder (self):
-        game_date = str(self.date.year) + "-" + str(self.date.month) + "-" + str(self.date.day)
-        score_board = scoreboardv2.ScoreboardV2(game_date=game_date)
+        score_board = scoreboardv2.ScoreboardV2(game_date=self.game_date)
         game_id = pd.DataFrame(score_board.get_data_frames()[0]).loc[:, "GAME_ID"]
         return game_id
 
     def gamestats (self, ids = pd.Series):
         for id in ids:
-            games = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=id)
-            game_stats = games.get_data_frames()[0]
-            print(game_stats)
+            box_score = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=id)
+            team_stats = box_score.get_data_frames()[1]
+            print(team_stats)
+        return 0
+
+    def playerstats (self, ids = pd.Series):
+        for id in ids:
+            box_score = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=id)
+            players_stats = box_score.get_data_frames()[0]
+            print(players_stats)
         return 0
 
 
 def main():
     today_game = TeamStats()
-    print(today_game.date)
     game_id = today_game.gamefinder()
-    print(game_id)
     today_game.gamestats(game_id)
     return today_game
 
